d9c24720dc2577f8a0ee5d63597a5c3b1eeeb958
Stan Chan <stanchan@gmail.com>
2019-10-28 11:45:44 -0700

    Update variables.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
-variable "aws_vpc_cidr_block" {
-  description = "CIDR Blocks for AWS VPC"
-}
+# variable "aws_vpc_cidr_block" {
+#   description = "CIDR Blocks for AWS VPC"
+# }
 
 variable "aws_cluster_name" {
   description = "Name of Cluster"
